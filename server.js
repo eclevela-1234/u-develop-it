ce28db65ec804b0163ceb07e9294396705bc661d
Eliot Cleveland <eclevela@richmond.edu>
2022-08-12 23:20:30 -0400

    code snapshot

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const mysql = require("mysql2");
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const inputCheck = require('./utils/inputCheck');
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "Ppinhead1951@",
-    database: "election",
-  },
-  console.log("Connected to the election database.")
-);
 
-//express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// GET all rows
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
-app.get("/api/candidates", (req, res) => {
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
   const sql = `SELECT * FROM candidates`;
 
   db.query(sql, (err, rows) => {
@@ -28,13 +32,14 @@ app.get("/api/candidates", (req, res) => {
       return;
     }
     res.json({
-      message: "Success!",
-      data: rows,
+      message: 'success',
+      data: rows
     });
   });
 });
 
-app.get("/api/candidate/:id", (req, res) => {
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
   const sql = `SELECT * FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
@@ -44,15 +49,14 @@ app.get("/api/candidate/:id", (req, res) => {
       return;
     }
     res.json({
-      message: "Success!",
-      data: row,
+      message: 'success',
+      data: row
     });
   });
 });
 
 // Delete a candidate
-
-app.delete("/api/candidate/:id", (req, res) => {
+app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
@@ -61,44 +65,52 @@ app.delete("/api/candidate/:id", (req, res) => {
       res.statusMessage(400).json({ error: res.message });
     } else if (!result.affectedRows) {
       res.json({
-        message: "Candidate not found",
+        message: 'Candidate not found'
       });
     } else {
       res.json({
-        message: "deleted",
+        message: 'deleted',
         changes: result.affectedRows,
-        id: req.params.id,
+        id: req.params.id
       });
     }
   });
 });
+
 // Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
 
-app.post('/api/candidate', ({body}, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({error: errors });
-        return;
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
     }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
+    res.json({
+      message: 'success',
+      data: body
     });
+  });
 });
 
-//default response for any other reqquest (not found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
